52494913b4bd1bd03924fd8205d110dcee23d753
JAISU1116564L$@ad.infosys.com <JAISU1116564L$@ad.infosys.com>
2018-11-23 12:21:51 +0530

    adddbcode

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Express = require('express'); 
 var multer = require('multer'); 
 var bodyParser = require('body-parser'); 
+var pg = require('pg');
 var app = Express(); 
 app.use(bodyParser.json()); 
 var Storage = multer.diskStorage({ 
@@ -18,6 +19,9 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html"); 
 }); 
  
+
+
+
 app.post("/api/Upload", function (req, res) { 
     upload(req, res, function (err) { 
         if (err) { 
@@ -25,6 +29,13 @@ app.post("/api/Upload", function (req, res) {
             return res.end("Something went wrong!"); 
         } 
         return res.end("File uploaded sucessfully!."); 
+        pg.connect("postgres://kkcoplxstuhduv:2e83c360f9c88bbf3f708960d6a6a3c99fd71dc6b49dcb3b93d1ce93b1f670de@ec2-23-21-201-12.compute-1.amazonaws.com:5432/d5f8ku16enksu0", function(err, client, done) {
+            client.query('SELECT * FROM DUEL', function(err, result) {
+              done();
+              if(err) return console.error(err);
+              console.log(result.rows);
+            });
+          });
     }); 
 }); 
  
