3b9d962494cbf05d4153844015084b8f1f7a0e9a
Alok Agrawal01 <Alok_Agrawal01@ad.infosys.com>
2018-11-23 21:38:49 +0530

    postgres chhanges

--- server.js
+++ server.js
@@ -1,49 +1,52 @@
-var Express = require('express'); 
-var multer = require('multer'); 
-var bodyParser = require('body-parser'); 
-var pg = require('pg');
-var app = Express(); 
-app.use(bodyParser.json()); 
-var Storage = multer.diskStorage({ 
-    destination: function (req, file, callback) { 
-        callback(null, "./Images"); 
-    }, 
-    filename: function (req, file, callback) { 
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname); 
-    } 
-}); 
- 
+var Express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: "postgres://kkcoplxstuhduv:2e83c360f9c88bbf3f708960d6a6a3c99fd71dc6b49dcb3b93d1ce93b1f670de@ec2-23-21-201-12.compute-1.amazonaws.com:5432/d5f8ku16enksu0",
+    ssl: true
+});
+
+var app = Express();
+app.use(bodyParser.json());
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
 var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count 
- 
-app.get("/", function (req, res) { 
-    res.sendFile(__dirname + "/index.html"); 
-}); 
- 
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
 
 
-app.post("/api/Upload", function (req, res) { 
-    upload(req, res, async (err) => { 
-        if (err) { 
-		console.log(err);
-            return res.end("Something went wrong!"); 
-        } 
-        
-       const client  = pg.Client("postgres://postgres:psadmin@localhost:5432/postgres") ;
-       await client.connect();
-       client.query('SELECT * FROM emp_data');
 
-        query.on('row', (row) => {
-            console.log(row);
-        });
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+            return res.end("Something went wrong!");
+        }
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM tsttable');
+            const results = { 'results': (result) ? result.rows : null };
+            client.release();
+        } catch (err) {
+            console.error(err);
+           // res.send("Error " + err);
+        }
+        res.send("File uploaded sucessfully!.");
+    });
+});
 
-        query.on('end', (row) => {
-            client.end();
-        });
-        return res.end("File uploaded sucessfully!.");         
-    }); 
-}); 
- 
-app.listen(process.env.PORT || 4000, function(){
+app.listen(process.env.PORT || 4000, function () {
     console.log('Your node js server is running');
 });
