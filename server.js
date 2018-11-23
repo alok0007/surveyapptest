5394fc4b617933aa48508275257e8cbeeac6a8db
JAISU1116564L$@ad.infosys.com <JAISU1116564L$@ad.infosys.com>
2018-11-23 18:06:29 +0530

    adddb

--- server.js
+++ server.js
@@ -23,20 +23,24 @@ app.get("/", function (req, res) {
 
 
 app.post("/api/Upload", function (req, res) { 
-    upload(req, res, function (err) { 
+    upload(req, res, async (err) => { 
         if (err) { 
 		console.log(err);
             return res.end("Something went wrong!"); 
         } 
-        pg.connect("postgres://kkcoplxstuhduv:2e83c360f9c88bbf3f708960d6a6a3c99fd71dc6b49dcb3b93d1ce93b1f670de@ec2-23-21-201-12.compute-1.amazonaws.com:5432/d5f8ku16enksu0", function(err, client, done) {
-            client.query('SELECT * FROM DUEL', function(err, result) {
-              done();
-              if(err) return console.error(err);
-              console.log(result.rows);
-            });
-          });
-        return res.end("File uploaded sucessfully!."); 
         
+       const client  = pg.Client("postgres://postgres:psadmin@localhost:5432/postgres") ;
+       await client.connect();
+       client.query('SELECT * FROM emp_data');
+
+        query.on('row', (row) => {
+            console.log(row);
+        });
+
+        query.on('end', (row) => {
+            client.end();
+        });
+        return res.end("File uploaded sucessfully!.");         
     }); 
 }); 
  
